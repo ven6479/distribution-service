25d5e2f9bda94ade012f21f545a8df4591e37165
Ven9 <108501422+ven6479@users.noreply.github.com>
2022-12-21 05:50:37 +0300

    Update admin.py

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Distribution, Client, Message,Contact
 
+
+admin.site.register(Distribution)
+admin.site.register(Client)
+admin.site.register(Message)
+admin.site.register(Contact)
 # Register your models here.
